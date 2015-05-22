39d420b1206c0c7f3c867ba513ed69c7e3909f27
unknown <ashu@pc-ashu-02.kyiv.ciklum.net>
2015-05-22 16:14:01 +0300

    94 of 100 for First home task

--- jsbursa1/index.js
+++ jsbursa1/index.js
@@ -1,12 +1,13 @@
 //Ваш код будет здесь
 function startNewGame() {
-  var winnerMessage = document.querySelector('.winner-message');
-    winnerMessage.innerHTML = '';
+    options.choice = 'x';
+    options.winnerMessage.innerHTML = '';
     var cellsX = document.querySelectorAll('.cell.x');
-  var cellsO = document.querySelectorAll('.cell.o');
+    var cellsO = document.querySelectorAll('.cell.o');
     clearClassNames(cellsX, 'x');
     clearClassNames(cellsO, 'o');
 
+    options.field.addEventListener('click', makeChoice);
 
 };
 
@@ -15,18 +16,40 @@ function clearClassNames(collection, className) {
         var elClassList = collection[i].classList;
         elClassList.remove(className);
     }
-}
+};
+
+function isEmptyCell(cellItem) {
+    return !( cellItem.classList.contains('x') || cellItem.classList.contains('o') );
+};
 
 function makeChoice(e) {
     var el = e.target;
-    el.classList.add('x');
+    if ( !isEmptyCell(el) ) return false;
+
+    if (options.choice == 'x') el.classList.add('x');
+    else el.classList.add('o');
+    options.choice = (options.choice == 'x') ? 'o' : 'x';
+
+    var winner = getWinner();
+    if (winner) finishGame(winner);
+
 };
 
+function finishGame(winner) {
+    var winnerText = (winner == 'x') ? 'Крестик победил' : 'Нолик победил';
+    options.winnerMessage.innerHTML = winnerText;
+    options.field.removeEventListener('click', makeChoice);
+}
+
 function init() {
-    var startBtn = document.querySelector('.startNewGame');
-    var field = document.querySelector('.field');
-    startBtn.addEventListener('click', startNewGame);
-    field.addEventListener('click', makeChoice);
+    options.choice = 'x';
+    options.winnerMessage = document.querySelector('.winner-message');
+    options.field = document.querySelector('.field');
+    options.startBtn = document.querySelector('.startNewGame');
+
+    options.startBtn.addEventListener('click', startNewGame);
 };
+
+var options = {}
 
 document.addEventListener('DOMContentLoaded', init);
