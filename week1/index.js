0c46f1f572da59e71f0cdc1f01a4962dabf2d442
Zascia <zascia@ukr.net>
2015-05-27 15:26:17 +0300

    linting first js homework

--- week1/index.js
+++ week1/index.js
@@ -1,55 +1,53 @@
-//Ваш код будет здесь
-function startNewGame() {
-    options.choice = 'x';
-    options.winnerMessage.innerHTML = '';
-    var cellsX = document.querySelectorAll('.cell.x');
-    var cellsO = document.querySelectorAll('.cell.o');
-    clearClassNames(cellsX, 'x');
-    clearClassNames(cellsO, 'o');
-
-    options.field.addEventListener('click', makeChoice);
-
-};
+// Ваш код будет здесь
+'use strict';
+var options = {};
 
 function clearClassNames(collection, className) {
-    for (var i = 0; i < collection.length; i++) {
-        var elClassList = collection[i].classList;
-        elClassList.remove(className);
-    }
-};
-
+  var i;
+  var elClassList;
+  for ( i = 0; i < collection.length; i++) {
+    elClassList = collection[i].classList;
+    elClassList.remove(className);
+  }
+}
 function isEmptyCell(cellItem) {
-    return !( cellItem.classList.contains('x') || cellItem.classList.contains('o') );
-};
-
+  return !( cellItem.classList.contains('x') || cellItem.classList.contains('o') );
+}
 function makeChoice(e) {
-    var el = e.target;
-    if ( !isEmptyCell(el) ) return false;
+  var el = e.target;
+  var winner;
+  if (!isEmptyCell(el)) return false;
 
-    if (options.choice == 'x') el.classList.add('x');
-    else el.classList.add('o');
-    options.choice = (options.choice == 'x') ? 'o' : 'x';
+  if (options.choice === 'x') el.classList.add('x');
+  else el.classList.add('o');
+  options.choice = (options.choice === 'x') ? 'o' : 'x';
 
-    var winner = getWinner();
-    if (winner) finishGame(winner);
-
-};
-
+  winner = getWinner();
+  if (winner) finishGame(winner);
+}
 function finishGame(winner) {
-    var winnerText = (winner == 'x') ? 'Крестик победил' : 'Нолик победил';
-    options.winnerMessage.innerHTML = winnerText;
-    options.field.removeEventListener('click', makeChoice);
+  var winnerText = (winner === 'x') ? 'Крестик победил' : 'Нолик победил';
+  options.winnerMessage.innerHTML = winnerText;
+  options.field.removeEventListener('click', makeChoice);
+}
+function startNewGame() {
+  var cellsX = document.querySelectorAll('.cell.x');
+  var cellsO = document.querySelectorAll('.cell.o');
+  options.choice = 'x';
+  options.winnerMessage.innerHTML = '';
+  clearClassNames(cellsX, 'x');
+  clearClassNames(cellsO, 'o');
+  options.field.addEventListener('click', makeChoice);
 }
 
 function init() {
-    options.choice = 'x';
-    options.winnerMessage = document.querySelector('.winner-message');
-    options.field = document.querySelector('.field');
-    options.startBtn = document.querySelector('.startNewGame');
+  options.choice = 'x';
+  options.winnerMessage = document.querySelector('.winner-message');
+  options.field = document.querySelector('.field');
+  options.startBtn = document.querySelector('.startNewGame');
 
-    options.startBtn.addEventListener('click', startNewGame);
-};
-
-var options = {}
+  options.startBtn.addEventListener('click', startNewGame);
+}
 
 document.addEventListener('DOMContentLoaded', init);
+
