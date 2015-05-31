76804ac87356b8a62b18cdb6239867fb27f49df2
zascia <zascia@ukr.net>
2015-05-31 22:02:06 +0300

    Week2 task3 subtask Render game field - cells rows

--- week2/krestiki_2_0/index.js
+++ week2/krestiki_2_0/index.js
@@ -1,6 +1,7 @@
 var options = {};
 var state = {
-  game: []
+  game: [],
+  started: false
 };
 function isCurrentGame() {
   var data = localStorage.getItem('game');
@@ -18,19 +19,50 @@ function startOptions() {
 }
 
 function isLegalNumber(num) {
-  if ( isNaN(num) || !isFinite(num) ) return showError();
-  if ( num > options.maxNum || num < options.minNum ) return showError();
-  return true;
+  if ( isNaN(num) || !isFinite(num) || num > options.maxNum || num < options.minNum ) return showHideError();
+  return showHideError(true);
 }
-function showError() {
+function showHideError(hide) {
   var errorMsgDiv = document.querySelector('.error-message');
+  if (hide) {
+    errorMsgDiv.innerHTML = '';
+    return true;
+  }
   errorMsgDiv.innerHTML = options.errorMessage;
   return false;
+}
+function renderField(num) {
+  var i;
+  //hide start game field
+  var startGameField = document.querySelector('.startGame');
+  var cellsContainer = document.querySelector('.field');
+  startGameField.style.display = 'none';
+  //show battle field
+  var mainGame = document.querySelector('.mainGame');
+  mainGame.style.display = 'block';
+  //save length of field
+  state.game.push(num);
+  //draw rows and cells
+  //make template of row with cells
+  var currentRow = document.createElement('div');
+  currentRow.classList.add('row');
+  for ( i = 0; i < num; i++ ) {
+    var currentCell = document.createElement('div');
+    currentCell.classList.add('cell');
+    currentRow.appendChild(currentCell);
+  }
+  //insert template
+  for ( i = 0; i < num; i++ ) {
+    cellsContainer.appendChild(currentRow.cloneNode(true));
+  }
+  //flag
+  state.started = true;
 }
 function generateField() {
   var num = parseInt( document.querySelector('.count').value );
   if ( isLegalNumber(num) ) {
     options.createField.removeEventListener('click', generateField);
+    renderField(num);
   }
 }
 function checkGame() {
@@ -38,7 +70,6 @@ function checkGame() {
     var startGame = document.querySelector('.startGame');
     startGame.style.display = 'none';
   }
-  alert('No current game');
 }
 window.addEventListener('load', function () {
   'use strict';
