328b9fbc8226580108c0ffdcda7ad8fa025bd716
zascia <zascia@ukr.net>
2015-06-01 10:49:24 +0300

    Week2 task3 --> adding moves x and o logic

--- week2/krestiki_2_0/index.js
+++ week2/krestiki_2_0/index.js
@@ -1,7 +1,8 @@
 var options = {};
 var state = {
   game: [],
-  started: false
+  started: false,
+  next: 'x'
 };
 function isCurrentGame() {
   var data = localStorage.getItem('game');
@@ -13,6 +14,8 @@ function isCurrentGame() {
 }
 function startOptions() {
   options.createField = document.querySelector('.generateField');
+  options.field = document.querySelector('.field');
+  options.winnerMessage = document.querySelector('.winner-message');
   options.errorMessage = 'Вы ввели некорректное число';
   options.minNum = 5;
   options.maxNum = 15;
@@ -33,17 +36,17 @@ function showHideError(hide) {
 }
 function renderField(num) {
   var i;
-  //hide start game field
+  // hide start game field
   var startGameField = document.querySelector('.startGame');
   var cellsContainer = document.querySelector('.field');
   startGameField.style.display = 'none';
-  //show battle field
+  // show battle field
   var mainGame = document.querySelector('.mainGame');
   mainGame.style.display = 'block';
-  //save length of field
+  // save length of field
   state.game.push(num);
-  //draw rows and cells
-  //make template of row with cells
+  // draw rows and cells
+  // make template of row with cells
   var currentRow = document.createElement('div');
   currentRow.classList.add('row');
   for ( i = 0; i < num; i++ ) {
@@ -51,17 +54,20 @@ function renderField(num) {
     currentCell.classList.add('cell');
     currentRow.appendChild(currentCell);
   }
-  //insert template
+  // insert template
   for ( i = 0; i < num; i++ ) {
     cellsContainer.appendChild(currentRow.cloneNode(true));
   }
-  //flag
+  // TODO --> move to separate function
+  // after render logic
   state.started = true;
+  options.field = document.querySelector('.field');
+  options.field.addEventListener('click', makeChoice);
 }
-function generateField() {
+function newField() {
   var num = parseInt( document.querySelector('.count').value );
   if ( isLegalNumber(num) ) {
-    options.createField.removeEventListener('click', generateField);
+    options.createField.removeEventListener('click', newField);
     renderField(num);
   }
 }
@@ -71,9 +77,32 @@ function checkGame() {
     startGame.style.display = 'none';
   }
 }
+
+function isEmptyCell(cellItem) {
+  return !( cellItem.classList.contains('x') || cellItem.classList.contains('o') );
+}
+function finishGame(winner) {
+  var winnerText = (winner === 'x') ? 'Крестик победил' : 'Нолик победил';
+  options.winnerMessage.innerHTML = winnerText;
+  options.field.removeEventListener('click', makeChoice);
+  state.started = false;
+}
+function makeChoice(e) {
+  var el = e.target;
+  var winner;
+  if (!isEmptyCell(el)) return false;
+
+  if (state.next === 'x') el.classList.add('x');
+  else el.classList.add('o');
+  state.next = (state.next === 'x') ? 'o' : 'x';
+
+  winner = getWinner();
+  if (winner) finishGame(winner);
+}
+
 window.addEventListener('load', function () {
   'use strict';
   startOptions();
   checkGame();
-  options.createField.addEventListener('click', generateField);
+  options.createField.addEventListener('click', newField);
 });
