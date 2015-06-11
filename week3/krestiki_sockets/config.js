8806e96e022557a5bce5b31c446aa478b0153ec5
zascia <zascia@ukr.net>
2015-06-11 08:10:33 +0300

    Added template for websockets tasks

---
+++ week3/krestiki_sockets/config.js
@@ -0,0 +1,7 @@
+var gameUrls = {
+    list: 'ws://jb3.smartjs.academy/games',
+    newGame: 'http://jb3.smartjs.academy/newGame',
+    gameReady: 'http://jb3.smartjs.academy/gameReady',
+    move: 'http://jb3.smartjs.academy/move',
+    surrender: 'http://jb3.smartjs.academy/surrender'
+};
