414607a4417129070d734d961dd81c55e08e7ab2
Zascia <zascia@ukr.net>
2015-09-04 18:49:59 +0300

    deutsch

--- week7/server_nikitas.js
+++ week7/server_nikitas.js
@@ -1,3 +1,84 @@
 /**
  * Created by ashu on 15-Jul-15.
  */
+//????? ?????.??? ?? ???? ???.??? ??,??? ? ????? ????????.
+var http = require('http');
+var _ = require('lodash');
+var server = http.createServer();
+var users = [
+  { id: '1', name: 'Illya Klymov', phone: '+380504020799', role: 'Administrator' },
+  { id: '2', name: 'Ivanov Ivan', phone: '+380670000002', role: 'Student', strikes: 1 },
+  { id: '3', name: 'Petrov Petr', phone: '+380670000001', role: 'Support', location: 'Kiev' }
+];
+
+server.on('request', function (req, res){
+  var data = '';
+  req.on('data',  function(chunk){
+    data += chunk.toString('UTF-8');
+  });
+  var path = req.method + ' ' + req.url;
+
+  var headers = {
+    'Content-type' : 'application/json'
+  };
+  if(path === 'GET / refreshAdmins'){
+    res.end();
+    return;
+  }
+
+
+  if(!req.headers['content-type'] || req.headers['content-type'].indexOf('application/json') === -1){
+    res.writeHead(401);
+    res.end();
+    return;
+  }
+
+  var id = path.split('/')[3];
+  var item;
+  if(id){
+    item = _.find(users, {id: id});
+    if(!item){
+      res.writeHead(404, headers);
+      res.end();
+      return;
+    }
+  }
+
+  if(path.indexOf('DELETE /api/users') === 0 && item){
+    users.splice(users.indexOf(item), 1);
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+
+  if(path === 'GET /api/users'){
+    res.writeHead(200, {'Content-type' : 'application/json'});
+    res.end(JSON.stringify(users));
+    return;
+  }
+
+  if(path.indexOf('PUT /api.users') === 0 && item){
+    _.assign(item, JSON.parse(data));
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+
+  if(path === 'POST /api/users' && item){
+    var newUser = JSON.parse(data);
+    if(!newUser.role){
+      newUser.role = 'Student';
+    }
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.writeHead(204, headers);
+    res.end(JSON.stringify(newUser));
+    return;
+  }
+});
+
+if(module.parent){
+  module.exports = server;
+} else {
+  server.listen(20007);
+}
--- week9/final/js/magic_hfish.js
+++ week9/final/js/magic_hfish.js
@@ -17,4 +17,63 @@ function applyFunction(index) {
     applyFunction(index + 1)
   }
 }
-applyFunction(bla)
+applyFunction(bla);
+
+// second fish
+function sequence(fns, cb) {
+  var count = 0;
+  function repeat(i) {
+    if (i === fns.length) {
+      cb();
+    } else {
+      fns[i](function(){
+        i++;
+        repeat(i);
+      });
+    }
+  }
+  repeat(count)
+}
+
+function parallel(fns, cb) {
+  var count = 0;
+  var i = 0;
+  fns.map(function (element) {
+    count++;
+    element(function (){
+      if (i === count - 1) {
+        cb();
+      } else {
+        i++;
+      }
+    });
+  });
+}
+
+// Nikitas fish
+function sequence(fns, cb) {
+
+  function createFunction(index) {
+    if (index < fns.length){
+      return fns[index].bind(this,createFunction(index + 1))
+    }
+    return cb
+  }
+  createFunction(0)();
+}
+function parallel(fns, cb) {
+  var result = [];
+  var i;
+  for (i = 0; i < fns.length; i++) {
+    (function(index) {
+      fns[index](function() {
+        if (result.indexOf(index) === -1) {
+          result.push(index);
+        }
+        if (result.length === fns.length) {
+          cb();
+        }
+      });
+    })(i);
+  }
+}
