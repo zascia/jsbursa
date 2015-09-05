112e4cb8d66b4e9d73fdbf83f62ffd6e65b946a1
zascia <zascia@ukr.net>
2015-09-05 11:00:03 +0300

    adding answers to questions below the main code

--- deutsch1/test.js
+++ deutsch1/test.js
@@ -4,8 +4,6 @@
 
 var SmavaTestManager = (function ($){
     var TestManager;
-     var TheTestManager;
-    // debugger;
     (function ($) {
         var instance;
 
@@ -15,7 +13,6 @@ var SmavaTestManager = (function ($){
             }
 
             instance = new TheTestManager(cfg);
-            //console.dir(instance);
             return instance;
         }
     }(jQuery));
@@ -23,12 +20,11 @@ var SmavaTestManager = (function ($){
 
 
     TheTestManager = function (cfg) {
-        // debugger;
         if(!(this instanceof TheTestManager)){
             return new TheTestManager();
         }
 
-        //this.init(cfg);
+        this.init(cfg);
 
     };
 
@@ -46,11 +42,39 @@ var SmavaTestManager = (function ($){
     TheTestManager.prototype = new managerPrototype();
 
     return TestManager;
-     //return TheTestManager;
 
 }(jQuery));
 
 var mgr = new SmavaTestManager();
 
-console.dir(mgr);
 console.log (mgr.init());
+
+
+/*
+
+
+ Q1: Assume that your company needs to provide possibility for users to download files using browser, files are static and don’t change often, they are always modified and updated by developer or other person having similar qualifications. How would you implement such a feature?
+
+ A1: I would use CDN for statics
+
+
+------------------------------------------
+
+
+ Q2.1: What is printed in the console and why? Please assume that jQuery is loaded without conflicts beforehand.
+ In console the empty string is printed as the 'name' variable is unaccessible in such implementation.
+ To invoke a function as a constructor one should use 'new FunctionName()' operator. In that case its context would be new object instance and there also would appear possibility to add properties through the 'prototype' and also use 'this' properly. because 'this' depends on context of invocation.
+ Also instance.init in IIFE for 'var TestManager' returned not the proper object but it's method and it couldn't reach the 'name' value
+
+ ------------------------------------------
+
+
+ Q2.2: How to adjust the code so that "TestManager" is printed to the console?
+- return instance instead of instance.init;
+
+- managerPrototype should transform from object notation to function and then use
+TheTestManager.prototype = new managerPrototype();
+
+
+ ------------------------------------------
+ */
