6fa8b6a2cc88c57ff9695b2668bc2f3c2ee9f6c7
Zascia <zascia@ukr.net>
2015-10-29 17:33:42 +0200

    adding binding saved directives with just attribute names

--- angular/module1/task1/main.js
+++ angular/module1/task1/main.js
@@ -2,5 +2,7 @@
  * Created by ashu on 29-Oct-15.
  */
 window.addEventListener('DOMContentLoaded', function() {
+  MyAngular.directive('module', function(el){console.log('module works', el);});
+  MyAngular.directive('click', function(el){console.log('click works', el);});
   MyAngular.$compile(document.body);
 });
--- angular/module1/task1/module.js
+++ angular/module1/task1/module.js
@@ -7,17 +7,22 @@ var MyAngular = (function() {
 
   return {
       directive: function(name, fn) {
-        _directives[name].push(fn);
+        _directives[name] = fn;
       },
 
       $compile: function domTreeWalk(node, scope) {
+        var scope = scope || this;
         // test for node el
         if (node && node.nodeType === 1) {
           // the first child node
           var child = node.firstElementChild;
           while (child) {
             // work with elem
-            console.log('elem', child.tagName);
+            for (var key in _directives) {
+              if (child.hasAttribute(key)) {
+                _directives[key].call(scope, child);
+              }
+            }
             // recursively walk through the elements
             domTreeWalk(child, scope);
             // go to the next node
