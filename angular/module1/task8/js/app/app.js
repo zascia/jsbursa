9fce48203bf7aaf03f082e1bee1f14366eb0fce9
Zascia <zascia@ukr.net>
2015-12-03 16:18:37 +0200

    DZ details

--- angular/module1/task8/js/app/app.js
+++ angular/module1/task8/js/app/app.js
@@ -2,7 +2,49 @@
 (function(){
   'use strict';
   angular.module('app', [])
-    .controller('MainCtrl', function($scope) {
+    .controller('MainCtrl', function($scope, socket) {
+      socket.addEventListener('message', function(e) {
+        console.log('message', e.data);
+      });
+
+      socket.addEventListener('open', function(e) {
+        console.log('start', e.data);
+      });
+
+      socket.addEventListener('error', function(e) {
+        console.log('error', e.data);
+      });
+
+      socket.addEventListener('close', function(e) {
+        console.log('close', e.data);
+      });
+    })
+    .directive('gamerList', function() {
+      return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+
+        },
+        template: '',
+        link: function($scope, $element, $attrs) {
+
+        }
+      }
+    })
+    .factory('socket', function($rootScope) {
+      var socket = new WebSocket('ws://rating.smartjs.academy/rating');
+      return socket;
+      /*return {
+        on: function(eventName, callback) {
+          socket.on(eventName, function() {
+            var args = arguments;
+            $rootScope.$apply(function() {
+              callback.apply(socket, args);
+            });
+          });
+        }
+      }*/
 
     });
 })();
