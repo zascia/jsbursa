ad4d77728c5a95a46a6d4a02a31237684f464b18
Zascia <zascia@ukr.net>
2015-12-04 14:19:11 +0200

    getting playesr with scores directive

--- angular/module1/task8/js/app/app.js
+++ angular/module1/task8/js/app/app.js
@@ -1,9 +1,16 @@
 /* eslint-disable */
 (function(){
   'use strict';
+
   angular.module('app', [])
-    .controller('MainCtrl', function($scope, socket) {
-      socket.addEventListener('message', function(e) {
+    .controller('MainCtrl', function($scope) {
+      // dummy data
+      $scope.users = [
+        {id: 1, name: 'Haha', points: 768},
+        {id: 2, name: 'Hihi', points: 423},
+        {id: 3, name: 'Hehe', points: 911}
+      ];
+      /*socket.addEventListener('message', function(e) {
         console.log('message', e.data);
       });
 
@@ -17,25 +24,55 @@
 
       socket.addEventListener('close', function(e) {
         console.log('close', e.data);
-      });
+      });*/
     })
-    .directive('gamerList', function() {
+    .directive('userTable', function(userService) {
       return {
         restrict: 'E',
         replace: true,
         scope: {
-
+          users: '='
         },
-        template: '',
+        template: ['<table>',
+                    '<tr><th>Id</th><th>User</th><th>Scores</th></tr>',
+                    '<tr ng-repeat="user in users | orderBy: \'-points\'">',
+                      '<td>{{user.id}}</td>',
+                      '<td>{{user.name}}</td>',
+                      '<td>{{user.points}}</td>',
+                    '</tr>',
+                  '</table>'].join(''),
         link: function($scope, $element, $attrs) {
-
+          userService.getUsers().then(function(result) {
+            $scope.users = result.data.records;
+          });
         }
       }
     })
-    .factory('socket', function($rootScope) {
+    .service('userService', function($http) {
+
+      this.getUsers = function() {
+
+        return $http({
+          "method": "get",
+          "url": 'http://rating.smartjs.academy/rating'
+        });
+
+        /** to do later --> implement without $http
+        var xhr;
+        xhr = new XMLHttpRequest();
+        xhr.open('get', 'http://rating.smartjs.academy/rating');
+        xhr.addEventListener('readystatechange', function() {
+          if ( xhr.readyState === 4 ) {
+            console.log('data received', xhr);
+          }
+        });*/
+      }
+
+    })
+    /*.factory('socket', function($rootScope) {
       var socket = new WebSocket('ws://rating.smartjs.academy/rating');
       return socket;
-      /*return {
+      /!*return {
         on: function(eventName, callback) {
           socket.on(eventName, function() {
             var args = arguments;
@@ -44,7 +81,11 @@
             });
           });
         }
-      }*/
+      }*!/
 
-    });
+    })*/;
+
+
+
+
 })();
